c29d1fb841106ab7a0edc45379f0865d5e62011d
Artur Martins <arturmartinspaiva@gmail.com>
2023-10-03 17:52:48 -0300

    Initial commit

---
+++ ex1/ex1.js
@@ -0,0 +1,20 @@
+let nome = new String();
+let salario = new Number();
+let idade = new String();
+let diploma = new Boolean();
+
+nome = new String("Artur");
+console.log(typeof nome);
+nome = new String("Ana Luísa");
+console.log(typeof nome);
+
+salario = new Number(650);
+console.log(typeof salario);
+salario = new Number(1300);
+console.log(typeof salario);
+
+idade = new String();
+console.log(typeof idade);
+
+idade = new String();
+console.log(typeof idade);
