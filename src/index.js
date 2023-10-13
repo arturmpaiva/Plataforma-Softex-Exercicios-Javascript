91f70023c8cce2121dea52ef1520c1d9f6a15e90
Artur Martins <arturmartinspaiva@gmail.com>
2023-10-13 13:48:23 -0300

    fixed: server error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const porta = 3000;
 
-app.listen(3984);
+app.listen(porta, function() {
+    console.log("Servidor rodando na porta", + porta)
+})
 
-app.get('projects', (request, response) => {
-    console.log('server ok');
-    return response.send('Hello World!');
+app.get('/projects', (request, response) => {
+    return response.send('Hello World!')
 });
